a9c1205147820b4e867c8a8224d6943a500cb41f
Tchoun <tchoun.mb@gmail.com>
2023-04-27 11:36:12 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+datalogger.onLogFull(function () {
+    basic.showString("Logfull")
+})
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Minutes)
 datalogger.mirrorToSerial(true)
 basic.forever(function () {
@@ -8,6 +11,14 @@ basic.forever(function () {
     false,
     true
     )
-    datalogger.log(datalogger.createCV("Temperature", dht11_dht22.readData(dataType.temperature)))
+    datalogger.log(datalogger.createCV("Temp.1", dht11_dht22.readData(dataType.temperature)))
+    dht11_dht22.queryData(
+    DHTtype.DHT22,
+    DigitalPin.P2,
+    true,
+    false,
+    true
+    )
+    datalogger.log(datalogger.createCV("Temp.2", dht11_dht22.readData(dataType.temperature)))
     basic.pause(30000)
 })
