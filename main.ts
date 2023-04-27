3134d00577b27656ea4098613a7c86ace20709e5
Tchoun <tchoun.mb@gmail.com>
2023-04-27 11:18:29 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Minutes)
+datalogger.mirrorToSerial(true)
 basic.forever(function () {
-	
+    dht11_dht22.queryData(
+    DHTtype.DHT22,
+    DigitalPin.P1,
+    true,
+    false,
+    true
+    )
+    datalogger.log(datalogger.createCV("Temperature", dht11_dht22.readData(dataType.temperature)))
+    basic.pause(30000)
 })
